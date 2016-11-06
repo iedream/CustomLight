c1240b56885044d81f33f075043ad5368984492c
Catherine Zhao <iedream@Catherines-MacBook-Pro.local>
2016-11-06 01:53:35 -0400

    first commit

---
+++ CustomLight/HueLight.h
@@ -0,0 +1,13 @@
+//
+//  HueLight.h
+//  CustomLight
+//
+//  Created by Catherine Zhao on 2016-11-05.
+//  Copyright © 2016 Catherine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HueLight : NSObject
+
+@end
---
+++ CustomLight/HueLight.m
@@ -0,0 +1,13 @@
+//
+//  HueLight.m
+//  CustomLight
+//
+//  Created by Catherine Zhao on 2016-11-05.
+//  Copyright © 2016 Catherine. All rights reserved.
+//
+
+#import "HueLight.h"
+
+@implementation HueLight
+
+@end
